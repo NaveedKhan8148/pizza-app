fcce79c0ff515d4321411864c9c95f1a29b231e9
naveedkhan <naveedkhan@gmail.com>
2023-07-28 22:57:52 +0500

    initial commit

---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const path = require('path');
+const ejs = require('ejs');
+const expressLayouts = require('express-ejs-layouts'); // Corrected variable name
+const app = express();
+const PORT = process.env.PORT || 8000;
+
+
+ // Corrected the middleware usage
+app.get('/', (req, res) => {
+    res.render('home');
+});
+
+app.use(expressLayouts);
+app.set('views', path.join(__dirname, 'resources/views'));
+app.set('view engine', 'ejs');
+
+
+
+app.listen(PORT, () => {
+    console.log(`The server is running on Port No ${PORT}`);
+});
