533b680e0ad077f843677e32bf97183d200a56fd
naveedkhan <naveedkhan@gmail.com>
2023-07-30 22:34:16 +0500

    creating a login page and registeration page

--- server.js
+++ server.js
@@ -5,18 +5,30 @@ const expressLayouts = require('express-ejs-layouts'); // Corrected variable nam
 const app = express();
 const PORT = process.env.PORT || 8000;
 
-app.use(express.static('public'))
- // Corrected the middleware usage
-app.get('/', (req, res) => {
-    res.render('home');
-});
 
+app.use(express.static('public'))
 app.use(expressLayouts);
 app.set('views', path.join(__dirname, 'resources/views'));
 app.set('view engine', 'ejs');
 
 
+ // Corrected the middleware usage
 
-app.listen(PORT, () => {
+ app.get('/', (req, res) => {
+    res.render('home');
+});
+
+
+ app.get('/cart',(req,res)=>{
+    res.render('coustomers/cart')
+  });
+
+  app.get('/login',(req,res)=>{
+   res.render('auth/login')
+ });
+ app.get('/register',(req,res)=>{
+   res.render('auth/register')
+ });
+ app.listen(PORT, () => {
     console.log(`The server is running on Port No ${PORT}`);
 });
